59ddd69f4ef77a3c88288464d48f43a3ea9c1071
Oto Hlincik <ohlincik@gmail.com>
2015-04-12 10:02:42 -0400

    Add the seed quote

--- app/app.js
+++ app/app.js
@@ -1,1 +1,44 @@
+var seed_quote = {
+  title: 'Project',
+  customer: 'Customer',
+  rate: 100,
+  pm_ratio: 0.2,
+  sections: [
+    {
+      title: 'Section 1',
+      hours: 16,
+      items: [
+        {
+          title: 'Item 11',
+          hours: 10
+        },
+        {
+          title: 'Item 12',
+          hours: 6
+        }
+      ]
+    },
+    {
+      title: 'Section 2',
+      hours: 20,
+      items: [
+        {
+          title: 'Item 21',
+          hours: 8
+        },
+        {
+          title: 'Item 22',
+          hours: 12
+        }
+      ]
+    }
+  ]
+};
+
 var app = angular.module('estimatr', []);
+
+app.controller('EstimatrController', function($scope) {
+
+  $scope.quote = seed_quote;
+
+});
